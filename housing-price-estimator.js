adfc0342358d55cfb14e27343e4cc3c25ee38012
Jason Jaskolka <jason.jaskolka@claritysecurity.com>
2025-08-20 16:11:21 -0700

    Use ml-random-forest

--- housing-price-estimator.js
+++ housing-price-estimator.js
@@ -1,44 +1,61 @@
-import fs from 'node:fs';
-import readline from 'node:readline';
+import csv from 'csvtojson';
+import { RandomForestRegression } from 'ml-random-forest';
+
+const PRICE_LABEL = 'price';
 
 class HousingPriceEstimator {
-    #data;
-    #labels;
-    #loadPromise;
+    #features = Object.freeze(['bedrooms', 'bathrooms', 'yearBuilt', 'livingArea', 'lotSize']);
+    #regression;
+    #trainingPromise;
 
-    constructor (labelsPath, dataPath) {
-        this.#loadPromise = Promise.all([
-            this.#readFirstLine(labelsPath).then(line => this.#labels = line.split(',')),
-            import(dataPath, { with: { type: 'json' } }).then(module => this.#data = module.default)
-        ]);
-    }
+    constructor (trainingPath, options = {
+        maxFeatures: this.#features.length,
+        nEstimators: 200,
+        replacement: false,
+        seed: 42
+    }) {
+        this.#trainingPromise = this.#load(trainingPath).then(({ data, rows }) => {
+            const labels = rows[0];
 
-    async #readFirstLine (filePath) {
-        const fileStream = fs.createReadStream(filePath);
-        const rl = readline.createInterface({
-            input: fileStream,
-            crlfDelay: Infinity
+            const priceData = rows.slice(1).map(row => row[labels.indexOf(PRICE_LABEL)]);
+
+            data.forEach((row, index) => console.log(row, priceData[index]));
+
+            this.#regression = new RandomForestRegression(options);
+            this.#regression.train(data, priceData);
+
+            console.log(this.#regression.featureImportance());
+
+            return data;
         });
-
-        let result;
-
-        // Get first line and close stream
-        for await (const line of rl) {
-            result = line;
-
-            rl.close();
-
-            break;
-        }
-
-        return result;
     }
 
-    run () {
-        this.#loadPromise.then(() => {
-            console.log(this.#labels);
+    #load (path) {
+        return csv({ noheader: true, output: 'csv' }).fromFile(path).then(rows => {
+            const labels = rows[0];
+
+            return {
+                data: rows.slice(1).map(row => this.#features.map(feature => Number(row[labels.indexOf(feature)]))),
+                rows
+            };
+        });
+    }
+
+    run (inputPath) {
+        const inputPromise = this.#load(inputPath);
+
+        return Promise.all([
+            this.#trainingPromise,
+            inputPromise,
+        ]).then(([data, { data: inputData }]) => {
+            return {
+                data,
+                predictions: this.#regression.predict(inputData)
+            };
         });
     }
 }
 
-new HousingPriceEstimator(process.argv[2], process.argv[3]).run();
+new HousingPriceEstimator(process.argv[2]).run(process.argv[3]).then(({ data, predictions }) => {
+    data.forEach((row, index) => console.log(row, predictions[index]));
+});
